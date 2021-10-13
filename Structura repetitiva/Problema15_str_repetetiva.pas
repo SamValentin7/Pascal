d85f2ae2ee795aba47af35407454de4feaeebfd4
Samciucov Valentin <samciucov.valentin@gmail.com>
2021-10-13 21:59:02 +0300

    .

---
+++ Structura repetitiva/Problema15_str_repetetiva.pas
@@ -0,0 +1,20 @@
+﻿program Problema15;
+
+var
+  m, n: word;
+  I, S: word;
+
+begin
+  writeln('Dati doua numere:');
+  readln(m, n);
+  I := 0;
+  S := 0;
+  repeat
+    S := S + m;
+    I := I + 1;
+  until I = n;
+  
+  writeln('Produsul este: ', S);
+  
+
+ end.
