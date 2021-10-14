1ddc354233c903e99b8ef3183570096cdf1bb963
Samciucov Valentin <samciucov.valentin@gmail.com>
2021-10-14 11:39:41 +0300

    Update Problema 17.pas

--- Structura repetitiva/Problema 17.pas
+++ Structura repetitiva/Problema 17.pas
@@ -1,22 +1,21 @@
-Program problema17;
-  var 
-    N : word;
-    I, S : word;
-  begin 
-    writeln('Dati un numar natural');
-    readln(N);
-    I := 0;
-    S := 0;
-    repeat 
-      N := N div 10;
-      I := I + 1;
-    until N = 0;
-    if I = 1 then 
-      begin
+program problema17;
+
+var
+  N: word;
+  I: word;
+
+begin
+  writeln('Dati un numar natural');
+  readln(N);
+  I := 0;
+  repeat
+    N := N div 10;
+    I := I + 1;
+  until N = 0;
+  if I = 1 then 
+  begin
     writeln('Numarul are o cifra');
-      end;
-   if I <> 1 then
-      begin
+  end
+  else
     writeln('Numarul are ', I, ' cifre');
-      end;
-  end.
+end.
