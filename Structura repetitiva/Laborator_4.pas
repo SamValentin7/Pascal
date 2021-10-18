2f017ee50b551f5e2a64181251b870bf81febb25
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-10-18 12:42:28 +0300

    Add files via upload

---
+++ Structura repetitiva/Laborator_4.pas
@@ -0,0 +1,17 @@
+﻿program patratul_cifrelor;
+
+var
+  D: word;
+  I: word;
+begin
+  I := I + 1;
+  for I := 100 to 999 do
+  begin
+    if I = (I div 10) * (I div 10) - (I mod 10) * (I mod 10) then 
+    begin
+      writeln(I);
+    end;
+  end;
+  I := I + 1;
+  
+ end.
