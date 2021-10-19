8ca4308bdcb5f40e3d2bced0243359d31277b521
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-10-19 13:39:23 +0300

    Add files via upload

---
+++ Structura repetitiva/ProblemaX.pas
@@ -0,0 +1,21 @@
+﻿program factori_primi;
+
+var
+  N: word;
+  I: word;
+  cont: word;
+
+begin
+  writeln('Dati un numar natural');
+  readln(N);
+  I := N;
+  cont := 2;
+  repeat
+    if (N mod cont) = 0 then begin
+      N := N div cont;
+      writeln(N)
+      end;
+      cont := cont + 1;
+    I := N;
+  until I = 1;
+end.
