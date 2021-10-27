9d3b0c02c03e24c84099e580e206f5c4e68e7400
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-10-27 09:17:32 +0300

    Add files via upload

---
+++ Abstractia datelor/Abstractia datelor.pas
@@ -0,0 +1,34 @@
+﻿type
+  zile = 1..31;
+  consum = array[zile] of real;
+
+var
+  oct: consum;
+  i: byte;
+  k: real;
+
+begin
+  for i := 1 to 31 do
+  begin
+    oct[i] := random(10);
+    write(oct[i]:2);
+  end;
+  writeln;
+  
+    {ex,1}
+  k := 0;
+  for i := 1 to 31 do
+    k := k + oct[i];
+  writeln('Consumul lunar de energie electrica a intreprinderii este ', k);
+  
+    {ex. 2} 
+  for i := 1 to 31 do
+    k := K / 31;
+  writeln('Consumul zilnic mediu este ', k);
+  
+    {ex. 3} 
+  write('Zilele in care consumul a fost mai mare de 500kw sunt ');
+  for i := 1 to 31 do
+    if oct[i] > 5 then write(i:3);
+
+end.
