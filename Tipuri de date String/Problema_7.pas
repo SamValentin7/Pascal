3711168fbcba3b9a0c0fe4d0471c30f716a3f853
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-11-08 21:42:42 +0200

    Add files via upload

---
+++ Tipuri de date String/Problema_7.pas
@@ -0,0 +1,12 @@
+﻿Program litere_mari;
+var
+  s:string;
+  i, r: integer;
+  begin
+    writeln('Dati sirul de caractere: ');
+    readln(s);
+    r:=0;
+    for i:=1 to length(s) do
+      if ord(s[i]) in [65..90] then r:=r+1;
+    writeln ('Sirul de caractere contine ', r , ' litere mari');
+  end.
