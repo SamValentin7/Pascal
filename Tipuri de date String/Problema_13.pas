1c5ed464041e5bf459f67cddf690a84474f36f42
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-11-08 21:57:29 +0200

    Add files via upload

---
+++ Tipuri de date String/Problema_13.pas
@@ -0,0 +1,19 @@
+﻿Program P13;
+var
+  s: string;
+  i, a: integer;
+  procedure DEL (var s:string; i, a: integer);
+  begin
+    Delete(s, i, a);
+  end;
+  begin
+    writeln('Dati sirul de caractere: ');
+    readln(s);
+    writeln('Dati pozitia de unde incepe stergerea: ');
+    readln(i);
+    writeln('Dati nr. de pozitii de sters: ');
+    readln(a);
+    Del(s, i, a);
+    writeln('Sirul este: ', s);
+    
+  end.
