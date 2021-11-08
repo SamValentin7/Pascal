147888f5473144abaa40d37fed73197f426ad3bc
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-11-08 21:35:54 +0200

    Add files via upload

---
+++ Tipuri de date String/Problema_5.pas
@@ -0,0 +1,18 @@
+﻿Program P5;
+var s:string;
+a:boolean;
+i:integer;
+begin
+  writeln('Dati sirul de caractere: ');
+  readln(s);
+  i:=length(s);
+  a:=true;
+  while (i>0) and (a=True) do begin
+    a:= (s[i] in ['a'..'z']) or (s[i] in ['A'..'Z']) or (s[i]='.');
+    i:=i-i;
+  end;
+  if a=True then 
+    write('Textul contine doar litere si punct')
+  else
+    write('Textul nu contine doar litere si punct');
+end.
---
+++ Tipuri de date String/Problema_6.pas
@@ -0,0 +1,17 @@
+﻿Program Consoane;
+var s:string;
+r:integer;
+function NrCons(s:string):integer;
+var i:integer;
+begin
+  for i:=1 to length(s) do begin
+    case uppercase(s[i]) of
+      'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
+      'R', 'S', 'T', 'V', 'W', 'X', 'Z', 'Y': r:= r+1;
+    end;
+  end;
+  NrCons:=r;
+  begin
+    weiteln('Numarul consoanelor este: ', r);
+  end;
+end.
