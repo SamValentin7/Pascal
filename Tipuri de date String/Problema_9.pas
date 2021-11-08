6c50f6ed7d23b881ed1fc2785cca719607538f79
Samciucov Valentin <90217118+SamValentin7@users.noreply.github.com>
2021-11-08 21:48:21 +0200

    Add files via upload

---
+++ Tipuri de date String/Problema_9.pas
@@ -0,0 +1,13 @@
+﻿program convertire;
+
+var
+  s: string;
+  i: integer;
+
+begin
+  writeln('Dati sirul de caractere: ');
+  readln(s);
+  for i := 1 to length(s) do
+    if ord(s[i]) in [65..90] then s[i] := chr(ord(s[i]) + 32);
+  writeln('Literele mari convertite in mici: ', s);
+end.
